6be30e08f578c7cd1c2c1c6fcf0c9c2f173cec8b
Gabriel Freitas <gabrielftts2000@gmail.com>
2026-08-07 17:35:57 -0300

    Refactor pegar_sessao and verificar_token functions

--- dependencies.py
+++ dependencies.py
@@ -8,29 +8,23 @@ from main import SECRET_KEY, ALGORITHM, oauth2_schema
 
 def pegar_sessao():
     
-#-----------------------------------------------------------------------------------------------------------------------
-#Se durante a execucao da funcao na qual o pegar_funcao esta relacionado (neste caso seria a funcao criar_conta) 
-# der algum erro, ele trava na linha de codigo yield sem realizar o close. Para arrumar isso, é necessario colocar dentro da estrutura try
-#-----------------------------------------------------------------------------------------------------------------------
-
     try:
-        Session = sessionmaker(bind=db) #ligacao da minha sessao com meu banco de dados criado no model.py
-        session = Session() #inicializo minha sessao com o banco de dados
-        yield session #pega o valor da sessao sem encerrar o funcionamento da funcao. 
+        Session = sessionmaker(bind=db) 
+        session = Session() 
+        yield session 
 
-    finally: #independente se deu certo ou errado o try, vamos realizar o session.close()
-        session.close() #fecha minha sessao apos retornar pro usuario
+    finally: 
+        session.close() 
 
 
-#dependencia para verificar token
+
 def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
     try:
-        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM) #decode -> decodificacao do token
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM) 
         id_usuario = dic_info.get("sub")
     except JWTError:
         raise HTTPException (status_code=401, detail="Acesso negado, verifique a validade do token")
-    #verificar se o token é valor
-    #extrair o ID do usuario do token
+
     usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
     if not usuario:
         raise HTTPException(status_code=401, detail="Acesso invalido")
