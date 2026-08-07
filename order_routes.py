d2f11c2a12825cb11728138aa7baacdd1a0e31e8
Gabriel Freitas <gabrielftts2000@gmail.com>
2026-08-07 17:50:56 -0300

    Refactor order routes for improved clarity and structure

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,3 @@
-#criacao de um roteador
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao, verificar_token
@@ -7,13 +6,10 @@ from model import Pedido, Usuario, ItemPedido
 from typing import List
 
 
-#passar duas variaveis: o caminho e tags
-# a criacao de prefixo auxilia em dividir as rotas para nao ter conflito com outras rotas
-order_router = APIRouter(prefix = "/pedidos", tags = ["pedidos"], dependencies=[Depends(verificar_token)]) #passando o dependecies no order_router faz com que ela seja aplicada para todas as rotas ao mesmo tempo
 
-#criacao da rota definindo o tipo de requisicao e o caminho a ser seguido
-#decorator: linha de codigo que vem antes de uma funcao com @ que atribui uma funcionalidade nova para a funcao que esta sendo criada
-#json: formato de resposta
+order_router = APIRouter(prefix = "/pedidos", tags = ["pedidos"], dependencies=[Depends(verificar_token)]) 
+
+
 @order_router.get("/")
 async def pedidos():
     """
@@ -21,22 +17,17 @@ async def pedidos():
     """
     return {"mensagem": "voce acessou a rota de pedidos"}
 
-#criando um pedido
+
 @order_router.post("/pedido")
-async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)): #o Depends informa que o session sera a resposta da funcao pegar_sessao, que entra como parametro
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)): 
     novo_pedido = Pedido(usuario=pedido_schema.id_usuario)
     session.add(novo_pedido)
     session.commit()
     return {"mensagem": f"Pedido criado com sucesso. ID do pedido: {novo_pedido.id_usuario}"}
 
-#tanto para pedidos quanto para criar pedidos nao precisamos passar a dependencia de criar token pois a unica exigencia é q eles sejam usuários
 
-#nessa proxima rota é necessario ter um nivel de acesso para fazer alteracoes no pedido
-@order_router.post("/pedido/cancelar/{id_pedido}") #podemos passar uma coisa na rota que vira como argumento da minha funcao
-async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)): #depends(vefirificar_token) me passa quem é o usuario
-    #usuario.admin = True
-    #usuario.id = pedido.usuario
-
+@order_router.post("/pedido/cancelar/{id_pedido}") 
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)): 
     pedido = session.query(Pedido).filter(pedido.id == id_pedido).first()
     if not pedido:
         raise HTTPException(status_code=400, detail='Pedido nao encontrado')
@@ -47,8 +38,8 @@ async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessa
     pedido.status = "CANCELADO"
     session.commit()
     return {
-        "Mensagem": f"Pedido numero {pedido.id} cancelado com sucesso", #o {pedido.id} foca o programa carregadar as informacoes do pedido
-        "pedido": pedido #ao forcar carregar as informacoes do pedido, podemos mostar o dicionario do pedido chamando o pedido como mensagem
+        "Mensagem": f"Pedido numero {pedido.id} cancelado com sucesso",
+        "pedido": pedido 
         }
 
 @order_router.get("/listar")
@@ -73,8 +64,7 @@ async def adicionar_item_pedido (id_pedido: int,
 
     if not usuario.admin and usuario.id != pedido.usuario:
         raise HTTPException(status_code=401, detail='Voce nao tem autorizacao para essa operacao')
-
-    #criando um item pedido no meu banco de dados
+           
     item_pedido = ItemPedido(item_pedido_schema.quantidade, 
                              item_pedido_schema.sabor,
                              item_pedido_schema.tamanho, 
@@ -111,10 +101,8 @@ async def remover_item_pedido (id_item_pedido: int,
         "pedido": pedido
     }
 
-@order_router.post("/pedido/finalizar/{id_pedido}") #podemos passar uma coisa na rota que vira como argumento da minha funcao
-async def finalizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)): #depends(vefirificar_token) me passa quem é o usuario
-    #usuario.admin = True
-    #usuario.id = pedido.usuario
+@order_router.post("/pedido/finalizar/{id_pedido}")
+async def finalizar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)): 
 
     pedido = session.query(Pedido).filter(pedido.id == id_pedido).first()
     if not pedido:
@@ -126,8 +114,8 @@ async def finalizar_pedido(id_pedido: int, session: Session = Depends(pegar_sess
     pedido.status = "FINALIZADO"
     session.commit()
     return {
-        "Mensagem": f"Pedido numero {pedido.id} finalizado com sucesso", #o {pedido.id} foca o programa carregadar as informacoes do pedido
-        "pedido": pedido #ao forcar carregar as informacoes do pedido, podemos mostar o dicionario do pedido chamando o pedido como mensagem
+        "Mensagem": f"Pedido numero {pedido.id} finalizado com sucesso", 
+        "pedido": pedido 
         }
 
 @order_router.get("/pedido/{id_pedido}")
